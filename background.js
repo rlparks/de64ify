637f2cac5b5089d823397564c936bd64fdc6fa0d
Rebecca Parks <rebecca.parks@uga.edu>
2023-12-11 22:53:27 -0500

    Initial commit

---
+++ background.js
@@ -0,0 +1,22 @@
+browser.contextMenus.create(
+    {
+        id: "decode-copy",
+        title: "Decode Base64",
+        contexts: ["selection"],
+    },
+    () => void browser.runtime.lastError
+);
+
+browser.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === "decode-copy") {
+        try {
+            const decodedText = atob(info.selectionText);
+            navigator.clipboard.writeText(decodedText);
+            if (decodedText.substring(0, 4) === "http") {
+                browser.tabs.create({ url: decodedText });
+            }
+        } catch (err) {
+            alert("Selection is not Base 64 encoded");
+        }
+    }
+});
